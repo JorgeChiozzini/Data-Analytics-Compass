e0f05bb89a3505d1ba2058af63c0bfc53bd4823f
Jorge Chiozzini <jchiozzini@gmail.com>
2024-03-11 18:28:04 -0300

    add parte desafio sprint 7

--- Sprint 7/desafio/s3_uploader.py
+++ Sprint 7/desafio/s3_uploader.py
@@ -1,11 +1,16 @@
 import boto3
-import csv
 from datetime import datetime
 import os
 
 # Função para carregar dados para o S3
 def upload_to_s3(file_path, bucket_name, storage_layer, data_origin, data_format, data_specification):
-    s3_client = boto3.client('s3')
+    s3_client = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_KEY"),
+    aws_session_token=os.getenv("AWS_SESSION_TOKEN"),
+    region_name='us-east-1'
+)
 
     # Obter a data de processamento
     processing_date = datetime.utcnow().strftime("%Y/%m/%d")
@@ -22,8 +27,8 @@ def load_csv_to_s3(file_path, bucket_name, storage_layer, data_origin, data_form
     upload_to_s3(file_path, bucket_name, storage_layer, data_origin, data_format, data_specification)
 
 # Caminhos dos arquivos CSV
-movies_file_path = "/dados/movies.csv"
-series_file_path = "/dados/series.csv"
+movies_file_path = "dados/movies.csv"
+series_file_path = "dados/series.csv"
 
 # Configurações do S3
 s3_bucket_name = "datalake-jorge"
