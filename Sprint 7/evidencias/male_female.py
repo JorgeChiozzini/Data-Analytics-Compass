8db906c4a1fa08be2d93b8b9f1aa75da4a8a1620
Jorge Chiozzini <jchiozzini@gmail.com>
2024-03-13 09:45:43 -0300

    correção lab glue sprint 7

--- Sprint 7/evidencias/male_female.py
+++ Sprint 7/evidencias/male_female.py
@@ -7,6 +7,8 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import sum, desc
 
 ## @params: [JOB_NAME, S3_INPUT_PATH, S3_TARGET_PATH]
 args = getResolvedOptions(sys.argv, ['JOB_NAME', 'S3_INPUT_PATH', 'S3_TARGET_PATH'])
@@ -35,18 +37,45 @@ df = df.toDF()
 
 df = df.withColumn("ano", df["ano"].cast("int"))
 
-sum_by_name_sex = df.groupBy("nome", "sexo").agg({"total": "sum", "ano": "first"})
+sum_by_name_sex = (
+    df.groupBy("nome", "sexo", "ano")
+      .agg(sum("total").alias("total_registracoes"))
+)
 
-max_female_name = (sum_by_name_sex.filter(df.sexo == "F")
-                                   .orderBy(desc("sum(total)"))
-                                   .first())
+max_female_name = (
+    sum_by_name_sex.filter(df.sexo == "F")
+                   .groupBy("nome")
+                   .agg(sum("total_registracoes").alias("max_total"))
+                   .orderBy(desc("max_total"))
+                   .first()
+)
 
-max_male_name = (sum_by_name_sex.filter(df.sexo == "M")
-                                 .orderBy(desc("sum(total)"))
-                                 .first())
+max_male_name = (
+    sum_by_name_sex.filter(df.sexo == "M")
+                 .groupBy("nome")
+                 .agg(sum("total_registracoes").alias("max_total"))
+                 .orderBy(desc("max_total"))
+                 .first()
+)
 
-print("Nome feminino com mais registros:", max_female_name["nome"], "com", max_female_name["sum(total)"], "registros em", max_female_name["first(ano)"])
-print("Nome masculino com mais registros:", max_male_name["nome"], "com", max_male_name["sum(total)"], "registros em", max_male_name["first(ano)"])
+max_female_year = (
+    sum_by_name_sex.filter((df.sexo == "F") & (sum_by_name_sex.nome == max_female_name["nome"]))
+                   .orderBy(desc("total_registracoes"))
+                   .first()["ano"]
+)
 
+max_male_year = (
+    sum_by_name_sex.filter((df.sexo == "M") & (sum_by_name_sex.nome == max_male_name["nome"]))
+                 .orderBy(desc("total_registracoes"))
+                 .first()["ano"]
+)
+
+print("Nome feminino com mais registros:", max_female_name["nome"],
+      "com", max_female_name["max_total"],
+      "registros em", max_female_year)
+
+print("Nome masculino com mais registros:", max_male_name["nome"],
+      "com", max_male_name["max_total"],
+      "registros em", max_male_year)
 
 job.commit()
